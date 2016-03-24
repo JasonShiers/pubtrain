abc1d6d724072ae46e12fd950494773a57ab4fd8
Jason Shiers <jason.shiers@gmail.com>
2016-03-24 16:43:17 +0000

    Complete training on dashboard

--- index.php
+++ index.php
@@ -3,26 +3,19 @@
 	// configuration
 	require("includes/config.php"); 
 
-	// get user's conference requests
-	$query = "SELECT c.id, c.confdate, c.title, c.location, c.days, r.lmendorse, "
-		. "r.approved FROM conferences c, requests r WHERE r.userid=? "
-		. "AND r.conf_id=c.id AND c.confdate >= ? ORDER BY c.confdate ASC";
-	$requests = query($query, $_SESSION["userid"], date("Y-m-d"));
-
-	// get user's line manager approval requests (if any)
-	$query = "SELECT c.id, r.id AS reqid, c.confdate, c.title, c.location, c.days, r.userid, "
-		. "u.firstname, u.lastname FROM conferences c, requests r, users u "
-		. "WHERE r.linemgr=? AND r.conf_id=c.id AND u.userid = r.userid "
-		. "AND r.lmendorse IS NULL AND c.confdate >= ? ORDER BY c.confdate ASC";
-	$lmapprovals = query($query, $_SESSION["userid"], date("Y-m-d"));
-
-	// get user's conference history (past 2 years)
-	$query = "SELECT c.id, c.confdate, c.title, c.location, c.days, r.attended, r.id AS req_id "
+	// get user's conference history
+	$query = "SELECT c.id, DATE_FORMAT(c.confdate, '%b %Y') AS confdate, c.title, c.location, c.days, r.attended, r.id AS req_id "
 		. "FROM conferences c, requests r WHERE r.userid=? AND r.conf_id=c.id "
-		. "AND c.confdate BETWEEN ? AND ? ORDER BY c.confdate ASC";
-	$history = query($query, $_SESSION["userid"], date("Y-m-d", strtotime("-2 years")), date("Y-m-d"));
+		. "AND c.confdate <= ? ORDER BY c.confdate DESC";
+	$confhistory = query($query, $_SESSION["userid"], date("Y-m-d"));
 
+	// get user's training history
+	$query = "SELECT DATE_FORMAT(tr.date, '%b %Y') AS date, tl.type, tl.catmask, tr.description, "
+		. "tr.internal_location, tr.internal_trainer, tr.total_days "
+		. "FROM trainingrecords tr, traininglibrary tl WHERE tr.userid = ? AND tr.trainingid = tl.trainingid "
+		. "ORDER BY tr.date DESC";
+	$trainhistory = query($query, $_SESSION["userid"]);
+	
 	// render table
-	render("templates/dashboard.php", ["history" => $history, "lmapprovals" => $lmapprovals, 
-		"requests" => $requests, "title" => "Dashboard"]);
+	render("templates/dashboard.php", ["trainhistory" => $trainhistory, "confhistory" => $confhistory, "title" => "Dashboard"]);
 ?>
--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,50 +1,234 @@
-<h4><b>My Conference History</b></h4>
-	<div class="conflist">
-		<table style="width: 100%">
-		<thead>
-			<tr>
-				<th class="left" style="width: 6em;">
-					Start Date
-				</th>
-				<th>
-					Title
-				</th>
-				<th>
-					Location
-				</th>
-				<th style="width: 3em;">
-					Days
-				</th>
-				<th class="right" style="width: 12em;">
-					Feedback
-				</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-				foreach ($history as $h)
-				{
-					print("<tr>");
-					print("<td>" . htmlspecialchars($h["confdate"]) . "</td>");
-					print("<td>" . htmlspecialchars($h["title"]) . "</td>");
-					print("<td>" . htmlspecialchars($h["location"]) . "</td>");
-					print("<td>" . htmlspecialchars($h["days"]) . "</td>");
-					if ($h["attended"]===null)
-					{
-						print("<td><a href=\"feedback.php?id=" . htmlspecialchars($h["req_id"]) . "\">Submit</td>");
-					}
-					else if ($h["attended"]==0)
-					{
-						print("<td>N/A</td>");
-					}
-					else
-					{
-						print("<td ><a style=\"font-weight: bold; color: forestgreen;\" href=\"feedbackreview.php?id=" . htmlspecialchars($h["req_id"]) 
-							. "&userid=" . htmlspecialchars($_SESSION["userid"]) . "\">Review</a></td>");
-					}
-					print("</tr>");
-				}
-			?>
-		</tbody>
-	</table>
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+	<div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="ConferenceHistory">
+			<h4 class="panel-title">
+				<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseConferenceHistory" aria-expanded="true" aria-controls="collapseConferenceHistory">
+					<b>My Conference History</b>
+				</a>
+			</h4>
+		</div>
+		<div id="collapseConferenceHistory" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="ConferenceHistory">
+			<div class="panel-body">
+				<table class="conflist paginated" style="width: 100%">
+					<thead>
+						<tr>
+							<th style="width: 6em;">
+								Start Date
+							</th>
+							<th>
+								Title
+							</th>
+							<th>
+								Location
+							</th>
+							<th style="width: 3em;">
+								Days
+							</th>
+							<th>
+								Feedback
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							foreach ($confhistory as $h)
+							{
+								print("<tr>");
+								print("<td>" . htmlspecialchars($h["confdate"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["title"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["location"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["days"]) . "</td>");
+								if ($h["attended"]==1)
+								{
+									print("<td ><a style=\"font-weight: bold; color: forestgreen;\" href=\"feedbackreview.php?id=" . htmlspecialchars($h["req_id"]) 
+										. "&userid=" . htmlspecialchars($_SESSION["userid"]) . "\">Review</a></td>");
+								}
+								else
+								{
+									print("<td>N/A</td>");
+								}
+								print("</tr>");
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="TrainingHistory">
+			<h4 class="panel-title">
+				<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTrainingHistory" aria-expanded="false" aria-controls="collapseTrainingHistory">
+					<b>My Training History</b>
+				</a>
+			</h4>
+		</div>
+		<div id="collapseTrainingHistory" class="panel-collapse collapse" role="tabpanel" aria-labelledby="TrainingHistory">
+			<div class="panel-body">
+				<table class="conflist paginated" style="width: 100%">
+					<thead>
+						<tr>
+							<th style="width: 6em;">
+								Start Date
+							</th>
+							<th>
+								Title and Description
+							</th>
+							<th>
+								Location
+							</th>
+							<th>
+								Trainer
+							</th>
+							<th style="width: 3em;">
+								Days
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							foreach ($trainhistory as $h)
+							{
+								print("<tr>");
+								print("<td>" . htmlspecialchars($h["date"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["type"]));
+								if ($h["description"] !== ""){
+									print("<br />" . htmlspecialchars($h["description"]));
+								}
+								print("</td>");
+								if ($h["internal_location"] == 0)
+								{
+									print("<td>External</td>");
+								}
+								else
+								{
+									print("<td>Internal</td>");
+								}									
+								if ($h["internal_trainer"] === 0)
+								{
+									print("<td>External</td>");
+								}
+								else if ($h["internal_trainer"] == 1)
+								{
+									print("<td>Internal</td>");
+								}
+								else
+								{
+									print("<td>N/A</td>");
+								}
+								print("<td>" . htmlspecialchars($h["total_days"]) . "</td>");
+								print("</tr>");
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading" role="tab" id="PublicationHistory">
+			<h4 class="panel-title">
+				<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapsePublicationHistory" aria-expanded="false" aria-controls="collapsePublicationHistory">
+					<b>My Publication History</b>
+				</a>
+			</h4>
+		</div>
+		<div id="collapsePublicationHistory" class="panel-collapse collapse" role="tabpanel" aria-labelledby="PublicationHistory">
+			<div class="panel-body">
+				<table class="conflist paginated" style="width: 100%">
+					<thead>
+						<tr>
+							<th style="width: 6em;">
+								Start Date
+							</th>
+							<th>
+								Title
+							</th>
+							<th>
+								Location
+							</th>
+							<th style="width: 3em;">
+								Days
+							</th>
+							<th>
+								Feedback
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							foreach ($confhistory as $h)
+							{
+								print("<tr>");
+								print("<td>" . htmlspecialchars($h["confdate"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["title"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["location"]) . "</td>");
+								print("<td>" . htmlspecialchars($h["days"]) . "</td>");
+								if ($h["attended"]==1)
+								{
+									print("<td ><a style=\"font-weight: bold; color: forestgreen;\" href=\"feedbackreview.php?id=" . htmlspecialchars($h["req_id"]) 
+										. "&userid=" . htmlspecialchars($_SESSION["userid"]) . "\">Review</a></td>");
+								}
+								else
+								{
+									print("<td>N/A</td>");
+								}
+								print("</tr>");
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
+<script>
+	$(document).ready(function(){
+		$('table.paginated').each(function() {
+				var currentPage = 0;
+				var numPerPage = 5;
+				var $table = $(this);
+
+				$table.bind('repaginate', function() {
+					// Show all rows
+					$table.find('tbody tr').show();
+					// Hide rows on pages before current page
+					$table.find('tbody tr:lt(' + currentPage * numPerPage + ')').hide();
+					// Hide rows on pages after current page
+					$table.find('tbody tr:gt(' + ((currentPage + 1) * numPerPage - 1) + ')').hide();
+				});
+
+				var numRows = $table.find('tbody tr').length;
+				var numPages = Math.ceil(numRows / numPerPage);
+
+				var $pager = $('<div class="pager"></div>');
+
+
+				// Append pager title
+				$('<span class="pager-title"> Page: </span>').appendTo($pager);
+
+				// Append page numbers
+				for (var page = 0; page < numPages; page++) {
+				  $('<span class="page-number"> ' + (page + 1) + '</span>')
+					// On click
+					.bind('click', {'newPage': page}, function(event) {
+						// Update currentPage
+						currentPage = event.data['newPage'];
+						// Repaginate
+						$table.trigger('repaginate');
+						// Reset active page number
+						$(this).addClass('active').siblings().removeClass('active');
+					}).appendTo($pager).addClass('clickable');
+				}
+
+				// Initially set first page to active
+				$pager.find('span.page-number:first').addClass('active');
+
+				// Insert pager div underneath table
+				$pager.insertAfter($table);
+
+				// Run initial pagination
+				$table.trigger('repaginate');
+			});
+	});
+</script>
