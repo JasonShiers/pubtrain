8567d6d20cc28cc405963ea9ae277bb8cbd4822e
Jason Shiers <jason.shiers@gmail.com>
2016-04-19 09:12:18 +0100

    Update login form

--- templates/login_form.php
+++ templates/login_form.php
@@ -1,19 +1,17 @@
 <form action="login.php" method="post">
 	<fieldset>
 		<h3>To continue, please log in</h3> 
-		<h4>using your Sygnature username and password</h4>
 		<img alt="Login" src="img/login.png" />
 		<?php if (isset($failed) && $failed == 1): ?>
 			<div class="alert alert-danger" role="alert">Username/password is incorrect. Please try again.</div>
 		<?php endif?>
 		<div class="form-group">
-			<div class="col-xs-4 col-xs-offset-4">
-				<input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+			<div class="text-center">
+				<input autofocus class="form-control" name="username" placeholder="Username" type="text" style="display: inline; width: 10em;" />
+				<span class="emailsign">@sygnaturediscovery.com</span>
 			</div>
-		</div>
-		<div class="form-group">
-			<div class="col-xs-4 col-xs-offset-4">
-				<input class="form-control" name="password" placeholder="Password" type="password"/>
+			<div class="text-center">
+				<input class="form-control" name="password" placeholder="Password" type="password" style="display: inline; width: 25.5em;" />
 			</div>
 		</div>
 		<div class="form-group">
